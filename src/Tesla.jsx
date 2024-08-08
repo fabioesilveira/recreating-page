23c399e92e0f06fbb6fc5a25709da2906ec33709
Fabio Silveira <fabio.eds@hotmail.com>
2024-08-07 22:28:35 -0700

    updatedCode

---
+++ src/Tesla.jsx
@@ -0,0 +1,3 @@
+function Tesla(params) {
+    
+}
---
+++ src/data/data.js
@@ -0,0 +1,59 @@
+export const teslaCars = [
+    {
+        model:"MODEL 3-2024",
+        price:"$49.130",
+        range:"341 miles",
+        paragraph:"As with most EVs, the Model 3 gains speed quickly, smoothly, and almost silently, with the electric motor providing a strong punch from a stop. And it's fast—extremely so in some trims. Like all Teslas, the Model 3 carries its battery under the floor, resulting in a low center of gravity. The 60 mph time for the Model 3 Long Range in our test was 4.1 seconds.",
+        images: ["https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-blue-659ebaabd4e71.jpg?crop=1xw:1xh;center,top&resize=980:*","https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-front-three-quarters-in-motion-659ebaaaaa10f.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-front-interior-659ebaa9e070d.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-rear-seat-659ebab040bc4.jpg?crop=1xw:1xh;center,top&resize=980:*"],
+        srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+        show: false
+    },
+    {
+        model:"MODEL S-2024",
+        price:"$74.630",
+        range:"405 miles",
+        paragraph:"With an electric motor dedicated to each of the front and rear axles, the Model S offers full-time all-wheel drive no matter which version you choose. The acceleration of the two models ranges from outstanding to unbelievable. The standard Model S has proven itself an agile sports sedan with well-controlled body motions and direct steering. The Tesla Model S we tested reached 60 mph in a blistering 2.4 seconds",
+        images: ["https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-107-1638811396.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-108-1638811396.jpg?crop=1xw:1xh;center,top&resize=980:*","https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-102-1638811395.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-112-1638811399.jpg?crop=1xw:1xh;center,top&resize=980:*"],
+        srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+        show: false
+    },
+   {
+    
+        model:"MODEL Y-2024",
+        price:"$54,130",
+        range:"310 miles",
+        paragraph:"The Model Y come with its interior is largely free of buttons and relies mostly on a large infotainment display positioned at the center of an uncluttered dashboard. Rear-wheel drive Standard Range models with 19-inch wheels carry driving range estimates as high as 260 miles. The all-wheel-drive Long Range offers 310 miles of range; the Performance model's estimated range drops to 285 miles. But the latter comes with 20-inch wheels.",
+        images:["https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-101-1592842279.jpg?crop=1xw:1xh;center,top&resize=980:*","https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-102-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-103-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-104-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*"],
+        srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+        show: false
+   },
+   {
+
+        model: "MODEL X-2024",
+        price: "$79,630",
+        range: "335 miles",
+        paragraph: "A stark and nearly buttonless interior design is classic Tesla. The driver faces a digital gauge display and a unique yoke-style steering controller, which is often frustrating to use and difficult if not impossible to get used to. At least a regular steering wheel is available as a no-cost option.Among other similarly sized electric SUVs, the Model X Long Range boasts the highest MPGe ratings from the EPA. That model has ratings of 107 city, 97 highway, and 102 MPGe combined.",
+        images: ["https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-101-656e382168a37.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-109-656e3821a0e49.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-117-656e3822b0ec0.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-114-656e37ef76212.jpg?crop=1xw:1xh;center,top&resize=980:*"],
+        srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+        show: false
+   },
+   {
+    model: "CYBERTRUCK 2024",
+    price: "$101.985",
+    range: '340 miles',
+    paragraph: "The 600-hp Dual Motor Cybertruck will reportedly reach 60 mph in just 3.9 ticks and have a top speed of 112 mph. On the road, the big Cybertruck feels more agile than expected. We drove a Founders Edition Beast model and found its combination of rear-wheel steering and variable ratio steer-by-wire system made for easier maneuvering in urban environments than a typical pickup truck.",
+    images: ["https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-102-65e894591b71d.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-beast-1105-6632475b014db.jpeg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-107-65e8945c693e2.jpg?crop=1xw:1xh;center,top&resize=980:*", "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-115-662a6d20c035b.jpg?crop=1xw:1xh;center,top&resize=980:*"],
+    srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+    show: false
+   },
+   {
+    model: "ROADSTER 2026",
+    price: "$200.000",
+    range: "620 miles",
+    paragraph: "For now, the Roadster is merely a concept, and in general, concepts change drastically during their development into production models. As shown, the Roadster concept features a removable glass panel roof, which you can stow in the trunk. It has four seats, but the rear ones look as though they may not fit adults, given the Roadster's dramatically angled roofline. Other than that, we expect to see Tesla's signature minimalist interior design repeated here.",
+    images: ["https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-108.jpg?resize=980:*", "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-105.jpg?resize=980:*", "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-104.jpg?resize=980:*", "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-111.jpg?resize=980:*"],
+    srcVideo: "https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR",
+    show: false
+   }
+
+];
